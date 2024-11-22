ea1345aa0f9e97737a37668371776a1ac8b55dd5
Lauren Keenan <19015831@uhi.ac.uk>
2024-11-22 12:12:30 +0000

    script works with android

--- Assets/Scripts/LinkedInButton.cs
+++ Assets/Scripts/LinkedInButton.cs
@@ -2,15 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+/*
 public class LinkedInButton : MonoBehaviour,  IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
     public void OnPointerClick(PointerEventData data) 
     {
-        Debug.Log("Quad pressed via mouse/touch!");
+        Debug.Log("Quad pressed via mouse/touch on mobile!");
     }
     public void OnPointerUp(PointerEventData data) {}
 
     public void OnPointerDown(PointerEventData data) {}
 
+}
+*/
+
+public class LinkedInButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for touches
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == transform)
+                    {
+                        Debug.Log("Quad touched!");
+                        OnQuadTouch();
+                    }
+                }
+            }
+        }
+    }
+
+
+    void OnQuadTouch()
+    {
+        GetComponent<Renderer>().material.color = Color.red; // Change color to red
+        Debug.Log("Quad was touched!");
+
+    }
+
+    // Event-based interaction for the button
+    public void OnPointerClick(PointerEventData data)
+    {
+        Debug.Log("Quad pressed via mouse/touch");
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        Debug.Log("Pointer Up pressed.");
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        Debug.Log("Pointer Down pressed.");
+    }
 }
