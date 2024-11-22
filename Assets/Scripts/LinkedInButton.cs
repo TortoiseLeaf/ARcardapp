6a9e6a230f0d63fa8da25d15ec0c3ecc4a46a296
Marta Grazia <23011076@uhi.ac.uk>
2024-11-22 10:04:39 +0000

    fixed pointer click not working

--- Assets/Scripts/LinkedInButton.cs
+++ Assets/Scripts/LinkedInButton.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class LinkedInButton : MonoBehaviour, IPointerClickHandler
+public class LinkedInButton : MonoBehaviour,  IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
-
     public void OnPointerClick(PointerEventData data) 
     {
         Debug.Log("Quad pressed via mouse/touch!");
     }
+    public void OnPointerUp(PointerEventData data) {}
+
+    public void OnPointerDown(PointerEventData data) {}
+
 }
