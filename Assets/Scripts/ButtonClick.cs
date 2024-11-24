b82f95d004f938432699a7fb0ceb991cb32ca1cb
Lauren Keenan <19015831@uhi.ac.uk>
2024-11-24 11:06:00 +0000

    tidy up branch

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -2,19 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-/*
-public class LinkedInButton : MonoBehaviour,  IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
-{
-    public void OnPointerClick(PointerEventData data) 
-    {
-        Debug.Log("Quad pressed via mouse/touch on mobile!");
-    }
-    public void OnPointerUp(PointerEventData data) {}
 
-    public void OnPointerDown(PointerEventData data) {}
-
-}
-*/
 
 public class ButtonClick : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
