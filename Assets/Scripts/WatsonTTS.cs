fc98b685d72dd4f7e79a65e3dc2bece0f64a2e5a
Marta Grazia <23011076@uhi.ac.uk>
2024-12-11 02:57:13 +0000

    added wav header function to correctly store the wav file so that encoding is compatible with unity

--- Assets/Scripts/WatsonTTS.cs
+++ Assets/Scripts/WatsonTTS.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,45 +6,80 @@ using UnityEngine.Networking;
 public class WatsonTTS : MonoBehaviour
 {
     [SerializeField] private string _apiKey = "";
-    [SerializeField] private string _apiUrl = "";   
+    [SerializeField] private string _apiUrl = "";
 
-    // Start is called before the first frame update
-    void Start()
+    public AudioPlayer audioPlayer;
+    void Awake()
     {
-        StartCoroutine(Upload());
+        audioPlayer = GetComponent<AudioPlayer>();
+        StartCoroutine(SynthesizeAndDownloadAudio());
     }
 
-    IEnumerator Upload()
+    IEnumerator SynthesizeAndDownloadAudio()
     {
-        UnityWebRequest www = UnityWebRequest.Post(_apiUrl, "{ \"text\": \" Watson test \"}", "application/json");        
-        
+        // Create a POST request to the Watson TTS API
+        UnityWebRequest www = UnityWebRequest.Post(_apiUrl, "{ \"text\": \" Watson should say squiggle \"}", "application/json");
         www.SetRequestHeader("Authorization", "Basic " + System.Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes("apikey:" + _apiKey)));
-        www.SetRequestHeader("Accept", "audio/wav");
+        www.SetRequestHeader("Accept", "audio/wav;codec=pcm;rate=44100");
 
         yield return www.SendWebRequest();
 
         if (www.result != UnityWebRequest.Result.Success)
         {
-            Debug.LogError(www.error);
+            Debug.LogError($"Error downloading audio: {www.error}");
         }
         else
         {
-            Debug.Log("Downloading Audio");
+            Debug.Log("Downloading audio...");
 
-            byte[] convertedAudioData = www.downloadHandler.data;
-
+            byte[] audioData = www.downloadHandler.data;
+            
             string audioFilePath = Path.Combine(Application.persistentDataPath, "synthetized_audio.wav");
 
-            File.WriteAllBytes(audioFilePath, convertedAudioData);
+            // Save the downloaded audio file
+            byte[] wavData = AddWavHeader(audioData, 44100, 1, 16);
+            File.WriteAllBytes(audioFilePath, wavData);        
 
-            Debug.Log("Audio saved");
-            
+            Debug.Log($"Audio saved at {audioFilePath}");
+
+            // Start the coroutine to play the saved audio file
+            Debug.Log("Starting PlayAudio coroutine...");
+            StartCoroutine(audioPlayer.PlayAudio(audioFilePath));
         }
-        
     }
-    // // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+
+    private byte[] AddWavHeader(byte[] pcmData, int sampleRate, int channels, int bitsPerSample)
+    {
+        int byteRate = sampleRate * channels * (bitsPerSample / 8);
+        int subchunk2Size = pcmData.Length;
+        int chunkSize = 36 + subchunk2Size;
+
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            using (BinaryWriter writer = new BinaryWriter(memoryStream))
+            {
+                // RIFF header
+                writer.Write(new[] { 'R', 'I', 'F', 'F' }); // Chunk ID
+                writer.Write(chunkSize); // Chunk size
+                writer.Write(new[] { 'W', 'A', 'V', 'E' }); // Format
+
+                // fmt subchunk
+                writer.Write(new[] { 'f', 'm', 't', ' ' }); // Subchunk1 ID
+                writer.Write(16); // Subchunk1 size
+                writer.Write((short)1); // Audio format (PCM)
+                writer.Write((short)channels); // Number of channels
+                writer.Write(sampleRate); // Sample rate
+                writer.Write(byteRate); // Byte rate
+                writer.Write((short)(channels * (bitsPerSample / 8))); // Block align
+                writer.Write((short)bitsPerSample); // Bits per sample
+
+                // data subchunk
+                writer.Write(new[] { 'd', 'a', 't', 'a' }); // Subchunk2 ID
+                writer.Write(subchunk2Size); // Subchunk2 size
+                writer.Write(pcmData); // PCM data
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
 }
