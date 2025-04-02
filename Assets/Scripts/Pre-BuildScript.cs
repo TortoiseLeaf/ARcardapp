4af1bf14b6c9f69793b1ba63c5b5c716a846301e
Lauren Keenan <19015831@uhi.ac.uk>
2025-04-02 11:28:38 +0100

    try catch log

--- Assets/Scripts/Pre-BuildScript.cs
+++ Assets/Scripts/Pre-BuildScript.cs
@@ -1,18 +1,25 @@
 using System.IO;
-using UnityEditor;
+using System;
 using UnityEngine;
 
 public class PreBuildConfig
 {
     public static void CreateConfig()
     {
-        string _linkedinApi = System.Environment.GetEnvironmentVariable("LINKEDIN_API");
-
-        if (!string.IsNullOrEmpty(_linkedinApi))
+        try
         {
-            string configFilePath = "Assets/StreamingAssets/config.json";
-            File.WriteAllText(configFilePath, $"{{ \"_linkedinApi\": \"{_linkedinApi}\" }}");
-            Debug.Log("Config file generated: " + configFilePath);
+            string _linkedinApi = System.Environment.GetEnvironmentVariable("LINKEDIN_API");
+
+            if (!string.IsNullOrEmpty(_linkedinApi))
+            {
+                string configFilePath = "Assets/StreamingAssets/config.json";
+                File.WriteAllText(configFilePath, $"{{ \"_linkedinApi\": \"{_linkedinApi}\" }}");
+                Debug.Log("Config file generated: " + configFilePath);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error generating config file: " + e);
         }
     }
 }
