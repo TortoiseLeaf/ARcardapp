f285f40293ee102ddd4298d3fba7041957614fd2
Lauren Keenan <19015831@uhi.ac.uk>
2025-04-23 17:38:39 +0100

    add brebuild.sh

---
+++ prebuild.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting prebuild process from shell script"
+echo "This wrapper script allows Unity Cloud Build to execute a hook"
+echo "Your C# IPreprocessBuildWithReport implementation will be automatically executed by Unity"
+exit 0
