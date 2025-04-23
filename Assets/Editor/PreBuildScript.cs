c8fa84349a917b68cc78ac085fa1c233b2aa9413
Lauren Keenan <19015831@uhi.ac.uk>
2025-04-23 14:00:59 +0100

    create credentials loader for generated build file

--- Assets/Editor/PreBuildScript.cs
+++ Assets/Editor/PreBuildScript.cs
@@ -10,7 +10,6 @@ public class PreBuildScript : IPreprocessBuildWithReport
 
 
     public int callbackOrder => 0; 
-    //public int callbackOrder { get { return 0; } }
 
     public void OnPreprocessBuild(BuildReport report)
     {
